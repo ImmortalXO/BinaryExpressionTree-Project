c9a51fe7f67e6d0cb5b45ada383f36336771a01e
ImmortalXO <nlh2576@email.vccs.edu>
2024-04-16 19:09:38 -0400

    Update code

--- BinaryExpressionTree-Project.cpp
+++ BinaryExpressionTree-Project.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <string>
+#include <stack>
+#include <cstring>
+#include <fstream>
+
+#include "binaryExpressionTree.h"
 
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int main() {
+    ifstream inFile("RpnData.txt");
+    ofstream outFile("RpnOutputData.txt");
+
+    if (!inFile.is_open() || !outFile.is_open()) {
+        cerr << "Error opening the input/output file. Try again.";
+        return 1;
+    }
+
+    string postfix;
+    while (getline(inFile, postfix)) {
+        binaryExpressionTree<string> tree;
+        tree.buildExpressionTree(postfix);
+        double evalResult = tree.evaluateExpressionTree();
+        outFile << postfix << " = " << evalResult << endl;
+    }
+
+    inFile.close();
+    outFile.close();
+
+    return 0;
+};
--- binaryExpressionTree.h
+++ binaryExpressionTree.h
@@ -1,21 +1,69 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <stack>
+#include <cstring>
+#include <sstream>
+
 using namespace std;
 
-template <typename Type>
-class binaryTreeType;
-
-template <typename Type>
+template <class elemType>
 struct nodeType {
-	Type info;
-	nodeType<Type>* lLink;
-	nodeType<Type>* rLink;
+	elemType info;
+	nodeType<elemType>* lLink;
+	nodeType<elemType>* rLink;
 };
 
-class binaryExpressionTree: public binaryExpressionTree
-{
+template <class elemType>
+class binaryTreeType {
 protected:
+	nodeType<elemType>* root;
 public:
+	virtual void search(const elemType& item) const = 0;
+	virtual void insert(const elemType& item) = 0;
+	virtual void deleteNode(const elemType& item) = 0;
 };
 
+class binaryExpressionTree : public binaryTreeType<string> {
+private:
+	double evaluateExpressionTree(nodeType<string>* p) {
+
+	};
+public:
+	void buildExpressionTree(string& postfix) {
+		stack<nodeType<string>*> stck;
+		char* expression = new char[postfix.length() + 1];
+		strcpy(expression, postfix.c_str());
+		char* token = strtok(expression, " ");
+		while (token != nullptr) {
+			if (isdigit(token[0])) {
+				nodeType<string>* newNode = new nodeType<string>;
+				newNode->info = token;
+				newNode->lLink = nullptr;
+				newNode->rLink = nullptr;
+				stck.push(newNode);
+			}
+			else if (token == "+" || token == "-" || token == "*" || token == "/") {
+				nodeType<string>* newNode = new nodeType<string>;
+				newNode->info = token;
+				newNode->rLink = stck.top();
+				stck.pop();
+				newNode->lLink = stck.top();
+				stck.pop();
+				stck.push(newNode);
+			}
+			else {
+				cerr << "Unsupported token";
+				return;
+			}
+		}
+		if (stck.size() != 1) {
+			std::cerr << "Error: Malformed expression" << std::endl;
+			return;
+		}
+
+		root = stck.top();
+		//istringstream iss(postfix);
+		//string token;
+	}
+};
